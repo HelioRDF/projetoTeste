db9ba8a6c00a15f17720d53810bdcb52fedf7344
Helio Franca <heliordf@hotmail.com>
2021-02-07 20:09:30 -0300

    Inclusão de BottomMenu em listview.builder

--- lib/View/ListViewBuild.dart
+++ lib/View/ListViewBuild.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:projetoTeste/Model/Pessoa.dart';
 
 class ListViewBuild extends StatefulWidget {
@@ -13,9 +14,11 @@ class _ListViewBuildState extends State<ListViewBuild> {
     Pessoa(nome: "Aline", idade: 27)
   ];
   int num = 1;
+  var _selecaoSubmenu = 0;
+  String _mensagem = "";
 
   _criarLista() {
-    while (num < 60) {
+    while (num < 10) {
       num++;
       _listaPessoas.add(
         Pessoa(nome: "Helio", idade: 32),
@@ -29,6 +32,14 @@ class _ListViewBuildState extends State<ListViewBuild> {
   _salvar(nome, idade) {
     setState(() {
       _listaPessoas.add(Pessoa(nome: nome.text, idade: int.parse(idade.text)));
+      _mensagem = "Pessoa salva com sucesso!";
+    });
+
+    Timer timer = new Timer(new Duration(seconds: 5), () {
+      debugPrint("Print after 5 seconds");
+      setState(() {
+        _mensagem = "";
+      });
     });
   }
 
@@ -40,72 +51,146 @@ class _ListViewBuildState extends State<ListViewBuild> {
 
   @override
   Widget build(BuildContext context) {
+    var _appBar = AppBar(
+      title: Text("Exemplo Input & List"),
+    );
+
+    // var _bottomNavigationBar =
+    var a = 1;
+    var alturaAppBar = _appBar.preferredSize.height;
+    var alturaStatusBar = MediaQuery.of(context).padding.top;
+    var alturaTela =
+        MediaQuery.of(context).size.height - alturaAppBar - alturaStatusBar;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Exemplo Input & List"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Card(
-                  child: TextField(
-                controller: _nome,
-                decoration: InputDecoration(
-                    labelText: "Nome", contentPadding: EdgeInsets.all(10)),
-                keyboardType: TextInputType.text,
-              )),
-              Card(
-                  child: TextField(
-                      controller: _idade,
-                      decoration: InputDecoration(
-                          labelText: "Idade",
-                          contentPadding: EdgeInsets.all(10)),
-                      keyboardType: TextInputType.number)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Card(
-                      shadowColor: Theme.of(context).accentColor,
-                      color: Theme.of(context).accentColor,
-                      margin: EdgeInsets.only(top: 10, left: 5),
-                      elevation: 15,
-                      child: FlatButton(
-                        child: Text("Salvar"),
-                        textColor: Colors.white,
-                        onPressed: () {
-                          _salvar(_nome, _idade);
-                        },
-                      )),
-                ],
+      appBar: _appBar,
+      body: _selecaoSubmenu == 0
+          ? Lista(alturaTela, _listaPessoas)
+          : Cadastro(_nome, _idade, alturaTela, _salvar, _mensagem),
+      bottomNavigationBar: SizedBox(
+        height: alturaTela * 0.12,
+        child: BottomNavigationBar(
+          currentIndex: _selecaoSubmenu,
+          backgroundColor: Colors.grey[100],
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.blue,
+          elevation: 10,
+          selectedFontSize: 16,
+          unselectedFontSize: 12,
+          onTap: (indice) {
+            setState(() {
+              _selecaoSubmenu = indice;
+              print(_selecaoSubmenu);
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list,
               ),
+              label: "Lista",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: "Adicionar"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Lista extends StatelessWidget {
+  var alturaTela;
+  var _listaPessoas;
+  Lista(this.alturaTela, this._listaPessoas);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              color: Colors.blueGrey[100],
+              height: alturaTela * 0.86,
+              padding: EdgeInsets.only(top: 5),
+              child: ListView.builder(
+                  itemCount: _listaPessoas.length,
+                  itemBuilder: (context, indice) {
+                    Pessoa pessoa = _listaPessoas[indice];
+                    int numPessoa = indice+1;
+
+                    return Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                            radius: 25,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: FittedBox(child: Text("User $numPessoa "  )),
+                            )),
+                        title: Text(pessoa.nome),
+                        subtitle: Text("Idade:" + pessoa.idade.toString()),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
+
+class Cadastro extends StatelessWidget {
+  var alturaTela;
+  var _nome;
+  var _idade;
+  var _mensagem;
+  Function(TextEditingController, TextEditingController) _salvar;
+  Cadastro(
+      this._nome, this._idade, this.alturaTela, this._salvar, this._mensagem);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: alturaTela * 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Card(
+              child: TextField(
+            controller: _nome,
+            decoration: InputDecoration(
+                labelText: "Nome", contentPadding: EdgeInsets.all(5)),
+            keyboardType: TextInputType.text,
+          )),
+          Card(
+              child: TextField(
+                  controller: _idade,
+                  decoration: InputDecoration(
+                      labelText: "Idade", contentPadding: EdgeInsets.all(5)),
+                  keyboardType: TextInputType.number)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
               Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Container(
-                    height: 350,
-                    child: ListView.builder(
-                        itemCount: _listaPessoas.length,
-                        itemBuilder: (context, indice) {
-                          Pessoa pessoa = _listaPessoas[indice];
-                          return Card(
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                radius: 25,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: FittedBox(child: Text("User:")),
-                                  )),
-                              title: Text(pessoa.nome),
-                              subtitle: Text("Idade:"+pessoa.idade.toString()),
-                            ),
-                          );
-                        })),
-              )
+                padding: const EdgeInsets.only(left: 5),
+                child: FlatButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    _salvar(_nome, _idade);
+                  },
+                ),
+              ),
             ],
           ),
-        ),
+          Text(
+            _mensagem,
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )
+        ],
       ),
     );
   }
